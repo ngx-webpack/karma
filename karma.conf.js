85bd53dbcdc37b5e530f4c6ab53095b13e3fe9ec
gsoos <soos.gabor86@gmail.com>
2017-05-25 22:12:04 +0200

    upgrade dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 'use strict';
-// console.log(require('./webpack.config'))
+
 let karmaConfig = require('./').karma(require('./webpack.config'), { useDlls: false });
 
 module.exports = (config) => {
